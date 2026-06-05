01d0a7e981b39e9c7445cdca29500b8569d3860f
Tanwar-Aastha <tanwaraastha89@gmail.com>
2026-06-05 23:42:52 +0530

    Streamlit app

--- app.py
+++ app.py
@@ -1,78 +1,70 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
 import pickle
-import os
 
-# get the directory of the current file to load the model and encoders
-DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+# Load the trained model
+model = tf.keras.models.load_model('model.h5')
 
-# load the trained model and encoders
-model = tf.keras.models.load_model(os.path.join(DIR_PATH, 'model.h5'))
+# Load the encoders and scaler
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
 
-with open(os.path.join(DIR_PATH, "gender_mapping.pkl"), "rb") as file:
-    gender_mapping = pickle.load(file)
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    onehot_encoder_geo = pickle.load(file)
 
-with open(os.path.join(DIR_PATH, "onehot_geography.pkl"), "rb") as file:
-    geo_encoder = pickle.load(file)
-
-with open(os.path.join(DIR_PATH, "scaler.pkl"), "rb") as file:
+with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
-# setting up the title and description of the app
-st.title("Customer Churn Prediction")
-st.write("Enter the customer details to predict if they are likely to churn or not.")
+## streamlit app
+st.title('Customer Churn PRediction')
 
-# User Input Fields
-geography = st.selectbox("Geography", geo_encoder.categories_[0])
-gender = st.selectbox("Gender", list(gender_mapping.keys()))
-age = st.slider("Age", 18, 95)
-balance = st.number_input("Balance")
-credit_score = st.number_input("Credit Score")
-estimated_salary = st.number_input("Estimated Salary")
-tenure = st.slider("Tenure (Years)", 0, 10)
-num_of_products = st.slider("Number of Products", 1, 4)
-has_cr_card = st.selectbox("Has Credit Card", ["Yes", "No"])
-is_active_member = st.selectbox("Is Active Member", ["Yes", "No"])
+# User input
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-# Preprocess the input data
+# Prepare the input data
 input_data = pd.DataFrame({
-    "Geography": [geography],
-    "Gender": [gender],
-    "Age": [age],
-    "Balance": [balance],
-    "CreditScore": [credit_score],
-    "EstimatedSalary": [estimated_salary],
-    "Tenure": [tenure],
-    "NumOfProducts": [num_of_products],
-    "HasCrCard": [1 if has_cr_card == "Yes" else 0],
-    "IsActiveMember": [1 if is_active_member == "Yes" else 0]
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
 })
 
-geography_encoded = geo_encoder.transform(input_data[["Geography"]]).toarray()
-geo_encoded_df = pd.DataFrame(geography_encoded, columns=geo_encoder.get_feature_names_out(["Geography"]))
+# One-hot encode 'Geography'
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
-input_data["Gender"] = input_data["Gender"].map(gender_mapping)
+# Combine one-hot encoded columns with input data
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-# Remove original Geography column
-input_data.drop("Geography", axis=1, inplace=True)
-
-# reordering the input data columns to match the order expected by the scaler and model
-input_data = input_data[scaler.feature_names_in_]
-
-# Scaling the input data
+# Scale the input data
 input_data_scaled = scaler.transform(input_data)
 
-# Predicting churn
+
+# Predict churn
 prediction = model.predict(input_data_scaled)
-prediction_probability = prediction[0][0]
+prediction_proba = prediction[0][0]
 
+st.write(f'Churn Probability: {prediction_proba:.2f}')
 
-if prediction_probability > 0.5:
-    st.write(f"The customer is likely to churn with a probability of {prediction_probability:.2f}.")
+if prediction_proba > 0.5:
+    st.write('The customer is likely to churn.')
 else:
-    st.write(f"The customer is unlikely to churn with a probability of {prediction_probability:.2f}.")
+    st.write('The customer is not likely to churn.')
